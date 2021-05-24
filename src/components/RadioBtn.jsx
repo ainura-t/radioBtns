870acdfa0449b7531f280ffda35424bd663ce47e
ainura-t <76243047+ainura-t@users.noreply.github.com>
2021-05-24 16:54:45 -0400

    Initial commit

---
+++ src/components/RadioBtn.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const RadioBtn = ({ labelName, id, isChecked, func }) => {
+  return (
+    <>
+      <label>{labelName}</label>
+      <input
+        type="radio"
+        name="location"
+        id={id}
+        checked={isChecked}
+        onChange={func}
+      />
+    </>
+  );
+};
+export default RadioBtn;
